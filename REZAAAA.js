02ea49b375e15565ce2558b9bd7957c29075e490
ROSA pier-alexandre <pier-alexandre.rosa@epitech.eu>
2023-02-11 14:38:26 +0100

    aa

--- REZAAAA.js
+++ REZAAAA.js
@@ -1,5 +1,5 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://azerty:<password>@rezaaaa.znr0dgo.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://azerty:azerty@rezaaaa.znr0dgo.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect(err => {
   const collection = client.db("test").collection("devices");
